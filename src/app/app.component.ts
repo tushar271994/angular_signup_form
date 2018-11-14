3f26d7744447dd30adcb8655397a714b68b2308e
Tushar <tushar.mahawar@finuno.com>
2018-11-14 13:40:06 +0530

    signupform

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DatepickerOptions } from 'ng2-datepicker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'tushar';
+export class AppComponent  implements OnInit{
+  model: any = {};
+  currentDate:any
+
+  constructor( ) { 
+  }
+
+  ngOnInit(){
+   }
+    
+  onSubmit(form:NgForm) {
+    console.log(form.value);
+    form.reset();
+    alert("SignUp Successfully");
+  }
 }
